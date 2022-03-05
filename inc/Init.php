862cb8f13ec5017903ff64c56e098bab88cfb385
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-05 14:43:21 +0700

    Add type inference in the codes

--- inc/Init.php
+++ inc/Init.php
@@ -6,10 +6,10 @@ final class Init {
     /**
      * Initialize a class.
      * 
-     * @param class $class      Class from the services array.
-     * @return class instance   New instance of a class.
+     * @param string $class      Class from the services array.
+     * @return BaseController instance   New instance of a class.
      */
-    private static function instantiate($class) {
+    private static function instantiate(string $class): BaseController {
 	    return new $class;
     }
 
@@ -18,7 +18,7 @@ final class Init {
      * 
      * @return array Full list of classes.
      */
-    public static function get_services() {
+    public static function get_services(): array {
         require_once PLUGIN_PATH . 'inc/Pages/Admin.php';
         require_once PLUGIN_PATH . 'inc/Base/Enqueue.php';
         require_once PLUGIN_PATH . 'inc/Base/SettingsLinks.php';
