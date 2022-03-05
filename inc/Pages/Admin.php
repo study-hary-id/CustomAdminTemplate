a29fea5dc7134a9019cf547781a213f47d03f24c
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-05 17:37:56 +0700

    Add methods that set the list of pages

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -19,4 +19,57 @@ class Admin extends BaseController {
 		    ->addSubPages($this->subpages)
 		    ->register();
     }
+
+	/**
+	 * Set the main pages of the plugin.
+	 *
+	 * @return void
+	 */
+    public function setPages() {
+	    $this->pages = array(
+		    array(
+			    'page_title' => 'Alecadd Plugin',
+			    'menu_title' => 'Alecadd',
+			    'capability' => 'manage_options',
+			    'menu_slug' => 'alecadd_plugin',
+			    'callback' => array($this->callbacks, 'adminDashboard'),
+			    'icon_url' => 'dashicons-store',
+			    'position' => 110
+		    )
+	    );
+    }
+
+	/**
+	 * Set the submenu of the main pages within the plugin.
+	 *
+	 * @return void
+	 */
+    public function setSubpages() {
+	    $this->subpages = array(
+		    array(
+			    'parent_slug' => 'alecadd_plugin',
+			    'page_title' => 'Custom Post Type',
+			    'menu_title' => 'CPT Manager',
+			    'capability' => 'manage_options',
+			    'menu_slug' => 'alecadd_cpt',
+			    'callback' => function () { echo '<h1>Custom Post Type</h1>'; }
+		    ),
+		    array(
+			    'parent_slug' => 'alecadd_plugin',
+			    'page_title' => 'Custom Taxonomy',
+			    'menu_title' => 'Taxonomies',
+			    'capability' => 'manage_options',
+			    'menu_slug' => 'alecadd_taxonomy',
+			    'callback' => function () { echo '<h1>Taxonomies Manager</h1>'; }
+		    ),
+		    array(
+			    'parent_slug' => 'alecadd_plugin',
+			    'page_title' => 'Custom Widgets',
+			    'menu_title' => 'Widgets',
+			    'capability' => 'manage_options',
+			    'menu_slug' => 'alecadd_widget',
+			    'callback' => function () { echo '<h1>Widgets Manager</h1>'; }
+		    )
+	    );
+    }
 }
