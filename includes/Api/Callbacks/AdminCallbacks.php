eada4d09025b963080cd45c3bbaadb39c8a973c3
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-26 15:53:30 +0700

    Create template callbacks

--- includes/Api/Callbacks/AdminCallbacks.php
+++ includes/Api/Callbacks/AdminCallbacks.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @package CustomAdmin
+ */
 
 require_once PLUGIN_PATH . 'includes/Base/BaseController.php';
 
@@ -7,5 +10,25 @@ class AdminCallbacks extends BaseController
 	public function adminDashboard()
 	{
 		return require_once "$this->plugin_path/templates/admin.php";
+	}
+
+	public function adminCustomPostType()
+	{
+		return require_once "$this->plugin_path/templates/cpt-manager.php";
+	}
+
+	public function adminTaxonomy()
+	{
+		return require_once "$this->plugin_path/templates/taxonomy.php";
+	}
+
+	public function adminWidget()
+	{
+		return require_once "$this->plugin_path/templates/widget.php";
+	}
+
+	public function alecaddOptionGroup( $input )
+	{
+		return $input;
 	}
 }
--- templates/admin.php
+++ templates/admin.php
@@ -1,3 +1,1 @@
 <h1>Alecadd Plugin</h1>
-<h2>Alecadd Plugin Subtitle</h2>
-<h3>Alecadd Plugin More Subtitle</h3>
