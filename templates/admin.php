8d3df26640f1d8024aeea2870b2ec296ca8201a6
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-26 18:30:36 +0700

    Update template shows settings fields

--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,9 @@
 <h1>Alecadd Plugin</h1>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+    <?php
+    settings_fields( 'alecadd_options_group' );
+    do_settings_sections( 'alecadd_plugin' );
+    submit_button();
+    ?>
+</form>
