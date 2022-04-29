fd1a771b945a37ad1a5e315794a6dddb91f49d2b
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-29 19:04:27 +0700

    Create managers property and set the values

--- includes/Base/class-base-controller.php
+++ includes/Base/class-base-controller.php
@@ -11,11 +11,18 @@ class Base_Controller
 	public $plugin_path;
 	public $plugin_url;
 	public $plugin;
+	public $managers;
 
 	public function __construct()
 	{
 		$this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
 		$this->plugin_url  = plugin_dir_url( dirname( __FILE__, 2 ) );
 		$this->plugin      = plugin_basename( dirname( __FILE__, 3 ) ) . '/custom-admin.php';
+
+		$this->managers = array(
+			'cpt_settings'      => 'Activate Custom Post Type',
+			'taxonomy_settings' => 'Activate Taxonomies',
+			'widget_settings'   => 'Activate Widgets'
+		);
 	}
 }
