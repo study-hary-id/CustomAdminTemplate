b0e1ebb29c51325f05eb91214b00621ae543b47c
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-29 19:05:27 +0700

    Update custom-general-settings template

--- templates/custom-general-settings.php
+++ templates/custom-general-settings.php
@@ -1,3 +1,11 @@
 <div class="wrap">
     <h1>Custom Settings</h1>
+    <?php settings_errors(); ?>
+    <form action="options.php" method="post">
+        <?php
+        settings_fields( 'custom_admin_settings' );
+        do_settings_sections( 'custom_admin_settings_general' );
+        submit_button();
+        ?>
+    </form>
 </div>
